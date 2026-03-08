f1413bd2658a708613c5fb47faca3e8177fa0798
RyanYovichin <yovichin.ryan@gmail.com>
2026-03-07 20:23:41 -0500

    updating main

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ def run_cam():
             print("Failed to grab frame")
             break
 
-        black_count = find_black(frame)
-        cv.putText(frame, f"Black: {black_count}", (20,40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        #black_count = find_black(frame)
+        #cv.putText(frame, f"Black: {black_count}", (20,40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         cv.putText(frame, f"Time: {timestamp}", (20,80), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
-        print(f"{timestamp} | Black pixels: {black_count}")
+        #print(f"{timestamp}")# | Black pixels: {black_count}")
         frame = detect_flags(frame)
         cv.imshow("Camera", frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
@@ -55,18 +55,15 @@ def run_cam():
     cv.destroyAllWindows()
 
 def detect_flags(frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-        edges = cv2.Canny(blurred, 50, 150)
+        contours, hierarchy = cv2.findContours(frame, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         flags = []
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for i in range(len(contours)):
             a = contours[i]
             x, y, w, h = cv2.boundingRect(a)
             cen = (x + w // 2, y + h // 2)
-            if frame[cen[1]][cen[0]][0] < 100:
+            if yellowfuck(cen, frame): #r > 150 and g > 150 and b < 100
                 flags.append(a)
-        
+
         for contour in flags:
             epsilon = 0.02 * cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, epsilon, True)
@@ -75,7 +72,17 @@ def detect_flags(frame):
             cv2.drawContours(frame, [approx], -1, (0, 255, 255), 2)
 
         return frame
+def yellowfuck(point, frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    lower_yellow = np.array([20, 100, 100])
+    upper_yellow = np.array([30, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    # mask[y, x] > 0 means that pixel is yellow
+    if mask[point[1], point[0]] > 0:
+        return True
+    return False
 
 def main():
     print("Hello World")
